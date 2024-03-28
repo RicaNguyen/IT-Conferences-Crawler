f7660db6461281ed3d4b81174bb9e689a1cc2898
Rica Nguyen <22810211@student.hcmus.edu>
2024-03-28 12:11:17 +0700

    query name, date, location in dev.event

--- my-app/app/api/route.ts
+++ my-app/app/api/route.ts
@@ -12,39 +12,78 @@ export async function GET(request) {
   const page = await browser.newPage();
 
   // Navigate the page to a URL
-  await page.goto("https://research.com/upcoming-conferences/computer-science");
+  await page.goto("https://dev.events/tech");
 
   try {
-    await page.waitForSelector("#rankingItems", {
+    await page.waitForSelector("#events", {
       timeout: 120000,
       visible: true,
     }); // Adjust the timeout as needed
 
     const tableData = await page.evaluate(async () => {
-      const divRankingItems = document.getElementById("rankingItems");
+      const divEventsItems = document.getElementById("events");
 
-      if (!divRankingItems) {
+      if (!divEventsItems) {
         return [];
       }
 
-      const listItem = divRankingItems.children;
+      const listItem = divEventsItems.children;
 
       let rs = [];
-      for (let i = 0; i < divRankingItems.children.length; i++) {
+      for (let i = 4; i < divEventsItems.children.length; i++) {
         // query h4 -> a, get text and href
         let objData: Record<string, any> = {
           id: "",
           name: "",
           link: "",
+          date: "",
+          location: "",
         };
 
-        const elementA = listItem.item(i)?.querySelector("a");
-        if (elementA) {
-          objData.link = elementA.getAttribute("href");
-          objData.name = elementA.textContent;
+        const elementTime = listItem.item(i)?.querySelector("time");
+        if (elementTime) {
+          let temp = (elementTime.textContent || "")
+            .split(/\n| /)
+            .filter((timeItem) => timeItem);
 
-          rs.push(objData);
+          if (temp.length === 3) {
+            objData.date = `${temp[0]} ${temp[1]}, 20${temp[2]}`;
+          }
         }
+        const elementLink = listItem.item(i)?.querySelector("a");
+        if (elementLink) {
+          objData.link =
+            "https://dev.events" + elementLink.getAttribute("href");
+          objData.name = elementLink.textContent;
+        }
+        const elementLocation = listItem.item(i)?.querySelector("h3");
+        if (elementLocation) {
+          objData.location = elementLocation.innerText;
+        } // const elementSpans = listItem.item(i)?.querySelectorAll("span");
+        // if (elementSpans) {
+        //   const elementBTextContent = elementSpans[1].textContent;
+        //   if (elementBTextContent) {
+        //     // tách chuỗi theo \n hoặc space
+        //     // https://stackoverflow.com/questions/650022/how-do-i-split-a-string-with-multiple-separators-in-javascript
+        //     // remove các chuỗi rỗng, chuỗi "-" bằng hàm filter
+        //     let temp = elementBTextContent
+        //       .split(/\n| /)
+        //       .filter((i: string) => i);
+        //     // vd: "08-04-2024 - 11-04-2024 -\n            Leuven\n" sẽ ra
+        //     // output  [
+        //     //      "08-04-2024",
+        //     //     "-",
+        //     //      "11-04-2024",
+        //     //      "-",
+        //     //      "Leuven"
+        //     //  ]
+        //     // let startDate =
+        //     objData.startDate = temp[0];
+        //     objData.endDate = temp[2];
+        //     objData.location = temp[4];
+        //   }
+        // }
+        rs.push(objData);
       }
 
       return rs;
